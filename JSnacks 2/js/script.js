985d4a814c40cb123e734f6b61a76608389a03bc
danielevergani <dvergani94@gmail.com>
2021-03-02 11:31:01 +0100

    jsnacks 2

--- JSnacks 2/js/script.js
+++ JSnacks 2/js/script.js
@@ -1,1 +1,15 @@
+var parola1 = prompt("scrivi una parola");
+
+var parola2 = prompt("scrivi un'altra parola");
+
+if (parola1.length > parola2.length){
+    console.log(parola1);
+    console.log(parola2);
+}
+else if (parola1.length < parola2.length) {
+    console.log(parola2);
+    console.log(parola1);
+}
+else{
+    console.log("le parole hanno stessa lunghezza");
 }
