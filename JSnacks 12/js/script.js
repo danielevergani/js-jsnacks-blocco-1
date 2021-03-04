60031e0ed427f018fa60079308eb3f4f49115068
danielevergani <dvergani94@gmail.com>
2021-03-04 15:22:08 +0100

    jSnacks 12 corretto

--- JSnacks 12/js/script.js
+++ JSnacks 12/js/script.js
@@ -1,21 +1,43 @@
-var nome = ["paolo", "marco", "giuseppe", "pedro", "franco", "peppino"];
+var numeri = [1, 34, 32, 6, 8, 2,];
 
-var nomeInvitato = prompt("come ti chiami");
+var totale = 0;
 
-var presente = false
-
-var i = 0;
-
-while ( presente == false && i<nome.length){
-    
-    if  (nomeInvitato == nome[i] ) {
-        presente = true
-        console.log("entra")
-    }  
-    
-    i++;
+for ( var i = 0; i<numeri.length; i++){
+    if ( i % 2 != 0 ){
+        totale += numeri[i]
+    }
 }
 
-if (presente ==false){
-    console.log("non puoi entrare");
+console.log(totale);
+
+// utente inserisce numeri
+
+var numeri2 = [];
+
+var numeroAggiunta = 0;
+
+var totale2 = 0;
+
+var continua = "si";
+
+while (continua == "si"){
+
+    do{
+    numeroAggiunta = parseInt(prompt("scrivi un numero"));
+    } while ( isNaN(numeroAggiunta) );
+
+    numeri2.push(numeroAggiunta);
+    console.log(numeri2);
+    continua = prompt("se vuoi aggiungere ancora un numero scrivi si").toLowerCase();
+    
 }
+
+console.log(numeri2);
+
+for ( var j = 0; j<numeri2.length; j++){
+    if ( j % 2 != 0 ){
+        totale2 += numeri2[j]
+    }
+}
+
+console.log(totale2);
